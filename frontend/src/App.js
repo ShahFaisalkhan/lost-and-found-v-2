7d62967e4e2910ab21e81f61ca9a51b5f2bded2d
ShahFaisalkhan <shahfaisalkhan1993@gmail.com>
2024-12-23 11:20:13 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -74,7 +74,7 @@ const AppContent = () => {
       if (isLoggedIn && token && !isValidToken(token)) {
         handleTokenExpiration();
       }
-    }, 50000); // Check every 50 seconds
+    }, 5000); // Check every 50 seconds
   
     return () => clearInterval(interval); // Cleanup on unmount
   }, [token, isLoggedIn]);
